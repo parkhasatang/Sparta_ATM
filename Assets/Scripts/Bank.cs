4591fd47146d6e62b6482f88d2ec65e1e4090a6a
scotcy <kimjh470@naver.com>
2023-12-13 16:49:10 +0900

    feat : 오류Ui창 구현
    todo : IntroScene에서 플레이어 이름값 받아오기

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class Bank : MonoBehaviour
 {
+    public UiManager uiManager;
     public Player MyPlayer;
     public TMP_Text Balance;
     int CurrentBalance = 50000;
@@ -23,18 +24,23 @@ public class Bank : MonoBehaviour
     }
 
 
-    public void WithdrawMoney(int amount)
+    public void WithdrawMoney(int amount) // 출금 Step2
     {
         if (CurrentBalance >= amount)
         {
             CurrentBalance -= amount ;
             MyPlayer.WithdrawMoney(amount);
             UpdateBalance();
-            
+        }
+        else
+        {
+            uiManager.BankToMoney.text = "";
+            uiManager.ErrorMsg.text = $"Not Enough Money";
+            uiManager.ErrorPopUp.SetActive(true);
         }
     }
 
-    public void TranferMoney(int amount)
+    public void TranferMoney(int amount) // 입금 Step3
     {
         CurrentBalance += amount;
         UpdateBalance();
